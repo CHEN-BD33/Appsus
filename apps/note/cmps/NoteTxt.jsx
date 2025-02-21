936078711d56af37b549560af75c0ba87e66aaff
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-02-21 18:38:23 +0200

    Add note text dynamoc component

--- apps/note/cmps/NoteTxt.jsx
+++ apps/note/cmps/NoteTxt.jsx
@@ -1,7 +1,7 @@
-export function noteTxt({ info, onChangeInfo }) {
+export function NoteTxt({ info, onChangeInfo }) {
     return (
         <div className='note-text'>
-            <textarea value={info.txt} onChange={(ev) => onChangeInfo({ txt: ev.target.value })} placeholder='Enter you text...' />
+            <textarea value={info.txt} onChange={(ev) => onChangeInfo({ txt: ev.target.value })} placeholder="Enter your text..." />
         </div>
     )
 }
