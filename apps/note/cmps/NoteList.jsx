fe9daf269f50ec3e49cc2ffa6db4eca7f5d6ad47
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-02-21 18:37:52 +0200

    Add notelist component

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,15 @@
-export function NoteList() {
-    return <div>note list</div>
+import { NotePreview } from "../cmps/NotePreview.jsx"
+
+export function NoteList({notes, onChangeInfo}) {
+    return (
+        <div className="note-list">
+        {notes.map(note => (
+            <NotePreview 
+                key={note.id} 
+                note={note}
+                onChangeInfo={onChangeInfo}
+            />
+        ))}
+    </div>
+    )
 }
