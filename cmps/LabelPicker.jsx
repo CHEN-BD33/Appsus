6f19defe1e96ad11bf3a5da46c671f4df71eb143
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-03-03 10:50:45 +0200

    Edit label picker

--- cmps/LabelPicker.jsx
+++ cmps/LabelPicker.jsx
@@ -1,46 +1,63 @@
 
-const { useState } = React
+// const { useState } = React
 
-export function LabelPicker({onChange}) {
-    const labels = [
-        {name:'Critical', picked: '✔'  , backgroundColor: '#eb5a46'},
-        {name:'Family', picked: '✔'  , backgroundColor: '#0078bf'}, 
-        {name:'Work', picked: '✔'  , backgroundColor: '#60bd4e'}  , 
-        {name:'Frinds', picked: '✔'  , backgroundColor: '#f3d700'} , 
-        {name:'Spam', picked: '✔'  , backgroundColor: '#ff9f1a'} , 
-        {name:'Memoreis', picked: '✔'  , backgroundColor: '#c376e1'} , 
-        {name:'Romantic', picked: '✔'  , backgroundColor: '#0099b7'}]
-    const [selectedLabels, setSelectedLabels] = useState([])
+// export function LabelPicker({ selectedLabels = [], onChangeLablels }) {
+//     const [isOpen, setIsOpen] = useState(false)
+//     // const [selectedLabels, setSelectedLabels] = useState([])
 
-    function toggleLabel(label) {
-        var updatedLabels
-        if (selectedLabels.includes(label)) {
-            updatedLabels = selectedLabels.filter(l => l !== label)
-        } else {
-            updatedLabels = [...selectedLabels, label]
-        }
-        setSelectedLabels(updatedLabels)
-        onChange(updatedLabels) 
-    }
+//     const labels = [
+//         { name: 'Critical', picked: '✔', backgroundColor: '#eb5a46' },
+//         { name: 'Family', picked: '✔', backgroundColor: '#0078bf' },
+//         { name: 'Work', picked: '✔', backgroundColor: '#60bd4e' },
+//         { name: 'Frinds', picked: '✔', backgroundColor: '#f3d700' },
+//         { name: 'Spam', picked: '✔', backgroundColor: '#ff9f1a' },
+//         { name: 'Memoreis', picked: '✔', backgroundColor: '#c376e1' },
+//         { name: 'Romantic', picked: '✔', backgroundColor: '#0099b7' }]
 
-    return (
-        <section className="label-picker">
-            <h3>Pick Labels:</h3>
-            <div className="label-container">
-            {labels.map(label => (
-                <button key={label.name} onClick={() => toggleLabel(label.name)} style={{backgroundColor: label.backgroundColor}}
-                >
-                    {label.name} {selectedLabels.includes(label.name) ? <span>{label.picked} </span> : ''}
-                </button>
-            ))}
-            </div>
+//     function toggleLabel(label) {
+//         let updatedLabels
+//         if (selectedLabels.includes(label)) {
+//             updatedLabels = selectedLabels.filter(l => l !== label)
+//         } else {
+//             updatedLabels = [...selectedLabels, label]
+//         }
+//         // setSelectedLabels(updatedLabels)
+//         onChangeLablels(updatedLabels)
+//     }
 
-        
-           
-        </section>
-    )
-}
+//     return (
+//         <section className="label-picker">
+//             {/* <h3>Pick Labels:</h3> */}
+//             <button onClick={() => {
+//                 e.stopPropagation()
+//                 e.preventDefult()
+//                 setIsOpen(!isOpen)
+//             }} className="label-picker-btn"><i className="fa-solid fa-tags"></i></button>
 
-//Show the selected labels on the email / note missing (Afthe the app
-//  email and App note)
+//             {isOpen && (
+//                 < div className="label-options">
+//             {labels.map(label => (
+//                 <button key={label.name} className={`label-option ${selectedLabels.includes(label.name) ? 'selected' : ''}`}  style={{ backgroundColor: label.backgroundColor }} onClick={() => { 
+//                     e.stopPropagation()
+//                     e.preventDefult()
+//                     toggleLabel(label.name)
+//                 }
+                      
+
+//             }}
+//                     >
+//             {label.name} {selectedLabels.includes(label.name) ? <span>{label.picked} </span> : ''}
+//         </button>
+//     ))
+// }
+//             </div >
+
+
+
+//         </section >
+//     )
+// }
+
+// //Show the selected labels on the email / note missing (Afthe the app
+// //  email and App note)
 
